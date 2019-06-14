a2bbb027ac2072ca94f98c8d5174d63eb8eba579
jinocenc <42550038+jinocenc@users.noreply.github.com>
2019-06-14 16:31:06 -0700

    ⚡ eslint functioning!

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,14 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-
+// const v = 2;
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
+  methods: {
+    chicken() {}
+  },
   render: h => h(App)
 }).$mount("#app");
