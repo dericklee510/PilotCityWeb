f0156786c4ef884b39069cb9d9ada6504452c8c2
Eric Reyes <ericjrreyes2@gmail.com>
2019-08-15 17:33:38 -0700

    :truck: nested header

--- src/App.vue
+++ src/App.vue
@@ -16,7 +16,7 @@
 <script lang="ts">
 import Vue from "vue"
 import "@/assets/scss/main.scss"
-import HeaderMain from "@/components/layout/header.vue"
+import HeaderMain from "@/components/layout/header/header.vue"
 import Component from "vue-class-component"
 import FooterMain from "./components/layout/footer.vue"
 @Component({
