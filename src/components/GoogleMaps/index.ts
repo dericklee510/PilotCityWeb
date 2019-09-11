6a3bc3049d0cfde2c51b3626ff8466b099d9e484
Eric Reyes <ericjrreyes2@gmail.com>
2019-09-10 18:16:06 -0700

    :heavy_plus_sign: google maps setup

--- src/components/GoogleMaps/index.ts
+++ src/components/GoogleMaps/index.ts
@@ -1,5 +1,9 @@
 import { MAPS_API_KEY } from '@/keys';
+import googleMaps from "@google/maps"
 
-export const googleMapsClient = require('@google/maps').createClient({
+export const googleMapsClient = googleMaps.createClient({
     key:MAPS_API_KEY
 })
+
+import AutoComplete from "./Autocomplete/AutoComplete.vue"
+export {AutoComplete}
