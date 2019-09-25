7de3c1dc08f404b9b4948dfdbb4c112381099612
Jerold Inocencio <jerold.inocencio@gmail.com>
2019-09-24 18:56:36 -0700

    :sparkles: started citizen-id (unf)

--- src/views/Test/test.vue
+++ src/views/Test/test.vue
@@ -3,22 +3,96 @@
     class="citizen-id"
     fluid
   >
-    <v-container
+    <v-container  
       fluid
-      style="margin-top: 75px;"
+      style="margin-top: 80px;"
     >
       <v-card class="pc-container">
         <v-row
-          class="pc-background citizen-id__header"
+          class=" citizen-id__header pc-background"
+          align="end"
         >
-          <v-col cols="12">
-            <v-img
-              src="@/assets/citizen-id.svg"
-              class="citizen-id__icon"
-            />
+          <img
+            src="@/assets/citizen-id.jpg"
+            class="citizen-id__icon pc-h-center"
+          >
+          <v-col
+            class="citizen-id__title"
+          >
+            <h1 class="text-center">
+              CREATE YOUR CITIZEN ID
+            </h1>
           </v-col>
-          <v-col class="citizen-id__title">
-            <h1>CREATE YOUR CITIZEN ID</h1>
+        </v-row>
+        <v-row
+          class="citizen-id__body "
+          justify="center"
+          align="center"
+        >
+          <!-- create selector component -->
+          <v-col cols="12">
+            <v-btn
+              id="citizen-id__select"
+              rounded
+              disabled
+            >
+              <h2 class="text-capitalize">
+                Citizen Type
+              </h2>
+            </v-btn>
+          </v-col>
+          <v-col cols="12">
+            <v-col cols="4">
+              <v-col
+                cols="12"
+                style="padding: 0;"
+              >
+                <v-btn
+                  id="citizen-id__type--teacher"
+                  block
+                  class=".pc-background--pink"
+                >
+                  <h2 class="text-capitalize">
+                    Teacher
+                  </h2>
+                </v-btn>
+              </v-col>
+              <v-col
+                cols="12"
+                style="padding: 0;"
+              >
+                <v-btn
+                  id="citizen-id__type--employer"
+                  block
+                >
+                  <h2 class="text-capitalize">
+                    Employer
+                  </h2>
+                </v-btn>
+              </v-col>
+              <v-col
+                cols="12"
+                style="padding: 0;"
+              >
+                <v-btn
+                  id="citizen-id__type--student"
+                  block
+                >
+                  <h2 class="text-capitalize">
+                    Student
+                  </h2>
+                </v-btn>
+              </v-col>
+            </v-col>
+          </v-col>
+          <v-col cols="12">
+            <v-btn
+              id="pc-button"
+              flat
+              outlined
+            >
+              NEXT
+            </v-btn>
           </v-col>
         </v-row>
       </v-card>
