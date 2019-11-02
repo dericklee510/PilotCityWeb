793df9bbda7f6f043622322966aa67a46777a188
Jerold Inocencio <jerold.inocencio@gmail.com>
2019-11-02 16:28:58 -0700

    :construction: attempted to make login default landing page

--- src/views/Login/routes.ts
+++ src/views/Login/routes.ts
@@ -6,10 +6,25 @@ const routes: RouteConfig[] = [
         path: "/login",
         name: "login",
         component: Login,
+        meta: {layout: "simple"},
         beforeEnter(to,from,next): void{
             AuthStore.user?next({name:'signup.profile'}):next()
         }
 
+    },
+    {
+        path: ``,
+        name: 'landing',
+        beforeEnter(to,from,next): void{
+            AuthStore.user?next({name:'signup.profile'}):next(`/login`)
+        }
+    },
+    {
+        path: `/*`,
+        name: `404`,
+        beforeEnter(to,from,next): void{
+            AuthStore.user?next({name:'signup.profile'}):next(`/login`)
+        }
     }
 ]
 export default routes
