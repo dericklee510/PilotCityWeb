0b2eaf6ee26af9df17edab7c0a78ada54ad43214
Eric Reyes <ericjrreyes2@gmail.com>
2019-11-06 20:55:45 -0800

    PcMultiInput

---
+++ src/components/inputs/PCmultiinput.vue
@@ -0,0 +1,26 @@
+<template>
+    <PCtextfield @input="handleinput"/>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Component from 'vue-class-component';
+import PCtextfield from './PCtextfield.vue'
+
+type VueEvent = {
+    name:string
+    type:string
+    source:string
+    payload:any[]
+}
+@Component({
+    components:{
+        PCtextfield
+    }
+})
+export default class PCmultiinput extends Vue{
+    handleinput(event:VueEvent){
+        this.$emit('input', event.payload[0])
+    }
+}
+</script>
