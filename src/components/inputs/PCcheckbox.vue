8c488210cd90022e2d8e030e8e545fbf49626990
Eric Reyes <ericjrreyes2@gmail.com>
2019-11-08 04:37:58 -0800

    checkboxes

--- src/components/inputs/PCcheckbox.vue
+++ src/components/inputs/PCcheckbox.vue
@@ -1,53 +1,38 @@
 <template>
-  <div class="pc-checkbox">
-    <v-checkbox
-      v-for="(option,index) in options"
-      :key="option+index"
-      v-model="selected"
-      :value="option"
-      :label="option"
-    />
-    <v-checkbox
-      v-if="other"
-      v-model="otherChecked"
-      :value="otherInput"
-    >
-      <template v-slot:label>
-        <v-text-field
-          v-if="other"
-          v-model="otherInput"
-          :disabled="otherChecked"
-        />
-      </template>
-    </v-checkbox>
-  </div>
+  <span class="pc-checkbox">
+    <v-checkbox v-for="(option,index) in options" :key="option+index" v-model="selected" :value="option" :label="option" />
+    <!-- <v-row align="center"> -->
+      <v-checkbox v-model="otherChecked"></v-checkbox>
+      <v-text-field :disabled="!otherChecked" v-model="otherInput" label="Other"></v-text-field>
+    <!-- </v-row> -->
+  </span>
 </template>
 
 <script lang="ts">
-    
+
 import Vue from 'vue'
 import 'reflect-metadata'
-import {Component, Prop, Watch} from 'vue-property-decorator' 
+import { Component, Prop, Watch } from 'vue-property-decorator'
 
 @Component
-export default class  extends Vue{
-    @Prop()
-    public checked!: string[]
-    @Prop()
-    public options!: any[]
-    @Prop()
-    public other!: boolean
-    public otherChecked: boolean = false
-    public otherInput: string = ""
-    public selected: any[] = []
-    get checkedOptions(){
-        if(this.other && this.otherChecked)
-            return [...this.selected,this.otherInput]
-        return this.selected
-    }
-    @Watch('checkedOptions')
-    onCheckedOptionsChanged(newval: any[]){
-        this.$emit('input',newval)
-    }
+export default class PCcheckbox extends Vue {
+  @Prop()
+  public checked!: string[]
+  @Prop()
+  public options!: any[]
+  @Prop({ default: false })
+  public other!: boolean
+  public otherChecked: boolean = false
+  public otherInput: string = ""
+  public selected: any[] = []
+  get checkedOptions() {
+    if (this.other && this.otherChecked)
+      return [...this.selected, this.otherInput]
+    return this.selected
+  }
+  @Watch('checkedOptions')
+  onCheckedOptionsChanged(newval: any[]) {
+    this.$emit('input', newval)
+  }
 }
 </script>
