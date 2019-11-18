9f9661eace614b7a0167d3cd28e3473b5805be49
Eric Reyes <ericjrreyes2@gmail.com>
2019-11-17 21:32:19 -0800

    :construction: new typings

--- src/views/Profile/PrivateProfile/Teacher/const.ts
+++ src/views/Profile/PrivateProfile/Teacher/const.ts
@@ -38,18 +38,5 @@ export class CONST extends Vue{
         'Transportation'
     ]
 
-    public PROGRAM_GRADES = [
-        '9',
-        '10',
-        '11',
-        '12'
-    ]
 
-    public PROGRAM_SEMS = [
-        'Fall',
-        'Winter',
-        'All-Year',
-        'Spring',
-        'Summer'
-    ]
 }
--- src/views/Profile/PrivateProfile/Teacher/types.ts
+++ src/views/Profile/PrivateProfile/Teacher/types.ts
@@ -1,27 +1,34 @@
-import { AutoCompleteAddress } from '@/components/GoogleMaps'
-import { IBellSchedule } from './components/BellScheduleInput/types'
-import { ICourses } from './components/CourseInput/types'
+import { CoursePrograms } from './components/CoursePrograms/types';
+import { ICourses } from './components/CourseInput/types';
+import { AutoCompleteAddress } from '@/components/GoogleMaps';
+import { IBellSchedule } from './components/BellScheduleInput/types';
 export interface TeacherProfile{
-    citizen: {
-        title: string;
-        first_name: string;
-        last_name: string;
+    citizen:{
+        title:string
+        first_name:string
+        last_name:string
     }
-    school: {
-        district: string;
-        name: string;
-        location: AutoCompleteAddress;
-        BellSchedules: IBellSchedule[];
+    school:{
+        district:string
+        name:string
+        location:AutoCompleteAddress
+        bellSchedules: IBellSchedule[]
     }
-    classroom: {
-        location: string;
-        phone_number: string;
-        extension: string;
-        preferred_communication: string;
-        available_equipment: string[];
+    classroom:{
+        location:string
+        phone_number:string
+        extension:string
+        preferredCommunication:string
+        available_equipment:string[]
     }
-    courses: {
-        school_year: string;
-        classSchedules: ICourses[];
+    courses:{
+        schoolYear:string
+        prepPeriod:string
+        classSchedules:ICourses
+    }
+    programDetails:{
+        coursePrograms:CoursePrograms[]
+        engagement_alternative:boolean
+        purchase_emp_product:string
     }
 }
