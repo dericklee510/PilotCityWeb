52e682b245db71967795683c7c1bfcc6fdbede85
Eric Reyes <ericjrreyes2@gmail.com>
2019-12-19 12:37:29 -0800

    :rotating_light: fixed lint and git errors

---
+++ src/views/Login/ForgotPassword/index.ts
@@ -0,0 +1,9 @@
+import ForgotPassword from './ForgotPassword.vue'
+import code from './PasswordRecoveryCode.vue'
+import email from './PasswordRecoveryEmail.vue'
+
+export {
+    ForgotPassword,
+    code,
+    email
+}
