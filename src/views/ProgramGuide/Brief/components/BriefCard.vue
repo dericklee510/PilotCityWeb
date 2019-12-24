8fcd5172e49792e553059c66c20c8d09b9c6152e
Prajit Saravanan <prajit.saravana@gmail.com>
2019-12-23 16:46:29 -0800

    edited the brief folder html

--- src/views/ProgramGuide/Brief/components/BriefCard.vue
+++ src/views/ProgramGuide/Brief/components/BriefCard.vue
@@ -1,14 +1,14 @@
-<template>
+<template >
   <v-card class="briefcard__container">
     <!-- <img src="" alt="" id="programbrief__cover"> -->
     <v-row>
-      <v-col cols="1">
+      <v-col cols="12" md="2" xl="1">
         <div id="briefcard__cover"></div>
       </v-col>
       <v-col>
           <v-col cols="12">
-              <div class="briefcard__header">REquest for Pilot (RFP)</div>
-              <span><input type="checkbox">I acknowledgeI have fully read and understand the included document</span>
+              <div class="briefcard__header">Request for Pilot (RFP)</div>
+              <span><input type="checkbox"> I acknowledgeI have fully read and understand the included document</span>
               <v-col style="padding:0"><button class="text-uppercase">confirm</button></v-col>
           </v-col>
       </v-col>
