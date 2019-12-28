74edf17fcb27377fe2b3c3b3bf1a5d36cf35ed41
Prajit Saravanan <prajit.saravana@gmail.com>
2019-12-28 02:19:29 -0800

    finished the final touch up and editing the buttons and all the font features.

--- src/views/ProgramGuide/Posthack/Posthack.vue
+++ src/views/ProgramGuide/Posthack/Posthack.vue
@@ -13,35 +13,70 @@
             <v-row justify="center" class="mr-auto ml-auto posthack__description">Now that you’ve received feedback from your employer, it is time to evolve, iterate or pivot your project if necessary. </v-row>
 
 
-            <v-row justify="left" class="posthack__firstcheck"> 
-            <div class="posthack__line1"><input class="posthack__check" id="posthack__check" type="checkbox">Update Business Model Canvas <img id="posthack__requiredbutton" src="@/assets/requiredsymbol.png"> </div>
-            <div><button><img class="posthack__exportimage" id="posthack__exporticon" src="@/assets/greenexport.png"></button></div>
+
+            <v-row id="posthack__check1">
+
+                <span class="posthack__wholeline"><input class="posthack__check" type="checkbox">Update Business Model Canvas 
+
+                <span class="posthack__error">*</span>
+                <span><button class="posthack__exporticon"><img src="@/assets/greenexport.png"></button></span>
+
+                </span>
+
             </v-row>
 
             <v-row>
-                <div class="posthack__description1">Description of business model canvas</div>
+
+                <span class="posthack__descriptionline">
+                Description of business model canvas
+                </span>
+
             </v-row>
 
 
 
-            <v-row justify="left" class="posthack__secondcheck"> 
-            <div class="posthack__line1"><input class="posthack__check" id="posthack__check" type="checkbox">Update One Sentence Pitch<img id="posthack__requiredbutton" src="@/assets/requiredsymbol.png"> </div>
-            <div><button><img class="posthack__exportimage" id="posthack__exporticon2" src="@/assets/greenexport.png"></button></div>
+            <v-row id="posthack__check2">
+
+                <span class="posthack__wholeline"><input class="posthack__check" type="checkbox">Update One Sentence Pitch   
+
+                <span class="posthack__error">*</span>
+                <span><button class="posthack__exporticon"><img src="@/assets/greenexport.png"></button></span>
+
+                </span>
+
             </v-row>
 
             <v-row>
-                <div class="posthack__description1">Description of one sentence pitch</div>
+
+                <span class="posthack__descriptionline">
+                Description of one sentence pitch
+                </span>
+
             </v-row>
 
 
-            <v-row justify="left" class="posthack__secondcheck"> 
-            <div class="posthack__line1"><input class="posthack__check" id="posthack__check" type="checkbox">Update Written Elevator Pitch <img id="posthack__requiredbutton" src="@/assets/requiredsymbol.png"> </div>
-            <div><button><img class="posthack__exportimage" id="posthack__exporticon" src="@/assets/greenexport.png"></button></div>
+
+            <v-row id="posthack__check2">
+
+                <span class="posthack__wholeline"><input class="posthack__check" type="checkbox">Update Written Elevator Pitch    
+
+                <span class="posthack__error">*</span>
+                <span><button class="posthack__exporticon"><img src="@/assets/greenexport.png"></button></span>
+
+                </span>
+
             </v-row>
 
             <v-row>
-                <div class="posthack__description1">Description of written elevator pitch</div>
+
+                <span class="posthack__descriptionline">
+                Description of written elevator pitch
+                </span>
+
             </v-row>
+
+
+
 
         
             <v-row>
