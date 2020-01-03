369c10336b2baf1b9d5d31ca114a187675253e73
Juxsta <ericjrreyes2@gmail.com>
2020-01-03 15:19:08 -0800

    :sparkles: business model canvas frontend (1/2)

--- src/views/ProgramGuide/Businessmodelcanvas/businessmodelcanvas_enter.vue
+++ src/views/ProgramGuide/Businessmodelcanvas/businessmodelcanvas_enter.vue
@@ -34,183 +34,10 @@
 
 
       <!-- COMPONENT -->
-      <v-row
-        justify="center"
-        class="businessmodelcanvas_enter__description__label mt-12 mr-auto ml-auto pl-2 mb-1"
-      >
-        <v-col
-          cols="8"
-          class=""
-        >
-          Problem
-        </v-col>
- 
-        <v-col cols="4">     
-          <v-row class="">
-            <div class="businessmodelcanvas_enter__ratinglabel pt-2 pr-3">
-              Rating
-            </div>
-            <v-rating />
-          </v-row>
-        </v-col>
-      </v-row>
-
-      <ValidationObserver v-slot="{invalid}">
-        <v-row
-          justify="center"
-          class="mr-auto ml-auto mb-12"
-        >
-          <ValidationProvider
-            v-slot="{errors,failedRules}"
-            name="Problem"
-            class="pt-1 pb-12 pl-5 pr-4 businessmodelcanvas_enter__paragraph"
-            rules="required|max:255"
-          >
-            <v-textarea
-              v-model="problem"
-              :error-messages="failedRules.max?`Must not be longer than 280 Characters`:errors"
-              counter
-              placeholder="My pitch is..."
-            />
-          </ValidationProvider>
-        </v-row>
-  
-  
-        <!-- COMPONENT -->
-        <v-row
-          justify="center"
-          class="businessmodelcanvas_enter__description__label mt-12 mr-auto ml-auto pl-2 mb-1"
-        >
-          <v-col
-            cols="8"
-            class=""
-          >
-            Solution
-          </v-col>
-   
-          <v-col cols="4">     
-            <v-row class="">
-              <div class="businessmodelcanvas_enter__ratinglabel pt-2 pr-3">
-                Rating
-              </div>
-              <v-rating />
-            </v-row>
-          </v-col>
-        </v-row>
-  
-        <v-row
-          justify="center"
-          class="mr-auto ml-auto mb-12"
-        >
-          <ValidationProvider
-            v-slot="{errors,failedRules}"
-            class="pt-1 pb-12 pl-5 pr-4 businessmodelcanvas_enter__paragraph"
-            rules="required|max:255"
-          >
-            <v-textarea
-              v-model="solution"
-              :error-messages="failedRules.max?`Must not be longer than 280 Characters`:errors"
-              counter
-              placeholder="My pitch is..."
-            />
-          </ValidationProvider>
-        </v-row>
-  
-  
-        <!-- COMPONENT -->
-        <v-row
-          justify="center"
-          class="businessmodelcanvas_enter__description__label mt-12 mr-auto ml-auto pl-2 mb-1"
-        >
-          <v-col
-            cols="8"
-            class=""
-          >
-            Innovation
-          </v-col>
-   
-          <v-col cols="4">     
-            <v-row class="">
-              <div class="businessmodelcanvas_enter__ratinglabel pt-2 pr-3">
-                Rating
-              </div>
-              <v-rating />
-            </v-row>
-          </v-col>
-        </v-row>
-  
-        <v-row
-          justify="center"
-          class="mr-auto ml-auto mb-12"
-        >
-          <ValidationProvider
-            v-slot="{errors,failedRules}"
-            class="pt-1 pb-12 pl-5 pr-4 businessmodelcanvas_enter__paragraph"
-            rules="required|max:255"
-          >
-            <v-textarea
-              v-model="innovation"
-              :error-messages="failedRules.max?`Must not be longer than 280 Characters`:errors"
-              counter
-              placeholder="My pitch is..."
-            />
-          </ValidationProvider>
-        </v-row>
-  
-        <!-- COMPONENT -->
-        <v-row
-          justify="center"
-          class="businessmodelcanvas_enter__description__label mt-12 mr-auto ml-auto pl-2 mb-1"
-        >
-          <v-col
-            cols="8"
-            class=""
-          >
-            Cost
-          </v-col>
-   
-          <v-col cols="4">     
-            <v-row class="">
-              <div class="businessmodelcanvas_enter__ratinglabel pt-2 pr-3">
-                Rating
-              </div>
-              <v-rating />
-            </v-row>
-          </v-col>
-        </v-row>
-  
-        <v-row
-          justify="center"
-          class="mr-auto ml-auto mb-12"
-        >
-          <ValidationProvider
-            v-slot="{errors,failedRules}"
-            rules="required|max:255"
-            class="pt-1 pb-12 pl-5 pr-4 businessmodelcanvas_enter__paragraph"
-          >
-            <v-textarea
-              v-model="cost"
-              :error-messages="failedRules.max?`Must not be longer than 280 Characters`:errors"
-              counter
-              placeholder="My pitch is..."
-            />
-          </ValidationProvider>
-        </v-row>
      
-
-
-        <v-col
-          class="mr-auto ml-auto"
-          cols="5"
-        >
-          <v-btn
-            class="businessmodelcanvas_enter__button mb-10"
-            :disabled="invalid"
-          >
-            SAVE
-          </v-btn>
-        </v-col>
-      </ValidationObserver>
+      <business-model-canvas-comp
+        v-model="canvas"
+      />
     </v-col>
   </v-row>
 </template>
@@ -223,16 +50,21 @@
 import Vue from 'vue'
 import Component from 'vue-class-component'
 import { ValidationProvider, ValidationObserver } from 'vee-validate'
+import {BusinessModelCanvas as BusinessModelCanvasComp} from "./components"
+import { BusinessModelCanvas } from '../../../store/Database/types/utilities'
 @Component({
   components:{
     ValidationProvider,
-    ValidationObserver
+    ValidationObserver,
+    BusinessModelCanvasComp
   }
 })
 export default class businessmodelcanvas_enter extends Vue{
-    problem:string = ""
-    solution:string = ""
-    innovation:string = ""
-    cost:string = ""
+  canvas:BusinessModelCanvas = {
+    problem:"",
+    solution:"",
+    innovation:"",
+    cost:""
+  }
 }
 </script>
--- src/views/ProgramGuide/Businessmodelcanvas/businessmodelcanvas_view2.vue
+++ src/views/ProgramGuide/Businessmodelcanvas/businessmodelcanvas_view2.vue
@@ -13,7 +13,9 @@
         <i
           id="businessmodelcanvas_view2__backbuttonicon"
           class="fas fa-chevron-left"
-        ><button class="businessmodelcanvas_view2__backbutton">Back</button></i>
+        >
+          <button class="businessmodelcanvas_view2__backbutton">Back</button>
+        </i>
       </v-row>
 
       <v-row
@@ -37,158 +39,12 @@
 
       <!-- <v-row class="mt-12 mr-auto ml-auto pl-5 mb-4 businessmodelcanvas_view2__description__label">Elevator Pitch</v-row> -->
 
-
-      <!-- COMPONENT -->
-      <v-row
-        justify="center"
-        class="businessmodelcanvas_view2__description__label mt-12 mr-auto ml-auto pl-2 mb-1"
-      >
-        <v-col
-          cols="8"
-          class=""
-        >
-          Problem
-        </v-col>
- 
-        <v-col cols="4">     
-          <v-row class="">
-            <div class="businessmodelcanvas_view2__ratinglabel pt-2 pr-3">
-              Rating
-            </div>
-            <i class="fas fa-star businessmodelcanvas_view2__ratedstar" />
-            <i class="fas fa-star businessmodelcanvas_view2__ratedstar" />
-            <i class="fas fa-star businessmodelcanvas_view2__ratedstar" />
-            <i class="fas fa-star businessmodelcanvas_view2__ratedstar" />
-            <i class="far fa-star businessmodelcanvas_view2__unratedstar" />
-          </v-row>
-        </v-col>
-      </v-row>
-
-      <v-row
-        justify="center"
-        class="mr-auto ml-auto mb-12"
-      >
-        <input
-          placeholder="My pitch is..."
-          disabled="true"
-          class="pt-1 pb-12 pl-5 pr-4 businessmodelcanvas_view2__paragraph"
-        >
-      </v-row>
-
-
-      <!-- COMPONENT -->
-      <v-row
-        justify="center"
-        class="businessmodelcanvas_view2__description__label mt-12 mr-auto ml-auto pl-2 mb-1"
-      >
-        <v-col
-          cols="8"
-          class=""
-        >
-          Solution
-        </v-col>
- 
-        <v-col cols="4">     
-          <v-row class="">
-            <div class="businessmodelcanvas_view2__ratinglabel pt-2 pr-3">
-              Rating
-            </div>
-            <i class="fas fa-star businessmodelcanvas_view2__ratedstar" />
-            <i class="fas fa-star businessmodelcanvas_view2__ratedstar" />
-            <i class="fas fa-star businessmodelcanvas_view2__ratedstar" />
-            <i class="fas fa-star businessmodelcanvas_view2__ratedstar" />
-            <i class="far fa-star businessmodelcanvas_view2__unratedstar" />
-          </v-row>
-        </v-col>
-      </v-row>
-
-      <v-row
-        justify="center"
-        class="mr-auto ml-auto mb-12"
-      >
-        <input
-          placeholder="My pitch is..."
-          disabled="true"
-          class="pt-1 pb-12 pl-5 pr-4 businessmodelcanvas_view2__paragraph"
-        >
-      </v-row>
-
-
-      <!-- COMPONENT -->
-      <v-row
-        justify="center"
-        class="businessmodelcanvas_view2__description__label mt-12 mr-auto ml-auto pl-2 mb-1"
-      >
-        <v-col
-          cols="8"
-          class=""
-        >
-          Innovation
-        </v-col>
- 
-        <v-col cols="4">     
-          <v-row class="">
-            <div class="businessmodelcanvas_view2__ratinglabel pt-2 pr-3">
-              Rating
-            </div>
-            <i class="fas fa-star businessmodelcanvas_view2__ratedstar" />
-            <i class="fas fa-star businessmodelcanvas_view2__ratedstar" />
-            <i class="fas fa-star businessmodelcanvas_view2__ratedstar" />
-            <i class="fas fa-star businessmodelcanvas_view2__ratedstar" />
-            <i class="far fa-star businessmodelcanvas_view2__unratedstar" />
-          </v-row>
-        </v-col>
-      </v-row>
-
-      <v-row
-        justify="center"
-        class="mr-auto ml-auto mb-12"
-      >
-        <input
-          placeholder="My pitch is..."
-          disabled="true"
-          class="pt-1 pb-12 pl-5 pr-4 businessmodelcanvas_view2__paragraph"
-        >
-      </v-row>
-
-      <!-- COMPONENT -->
-      <v-row
-        justify="center"
-        class="businessmodelcanvas_view2__description__label mt-12 mr-auto ml-auto pl-2 mb-1"
-      >
-        <v-col
-          cols="8"
-          class=""
-        >
-          Cost
-        </v-col>
- 
-        <v-col cols="4">     
-          <v-row class="">
-            <div class="businessmodelcanvas_view2__ratinglabel pt-2 pr-3">
-              Rating
-            </div>
-            <i class="fas fa-star businessmodelcanvas_view2__ratedstar" />
-            <i class="fas fa-star businessmodelcanvas_view2__ratedstar" />
-            <i class="fas fa-star businessmodelcanvas_view2__ratedstar" />
-            <i class="fas fa-star businessmodelcanvas_view2__ratedstar" />
-            <i class="far fa-star businessmodelcanvas_view2__unratedstar" />
-          </v-row>
-        </v-col>
-      </v-row>
-
-      <v-row
-        justify="center"
-        class="mr-auto ml-auto mb-12"
-      >
-        <input
-          placeholder="My pitch is..."
-          disabled="true"
-          class="pt-1 pb-12 pl-5 pr-4 businessmodelcanvas_view2__paragraph"
-        >
-      </v-row>
-
-
+      <business-model-canvas-comp
+        v-model="canvas"
+        :stars="stars"
+        readonly
+        @starsChanged="onStarsChanged"
+      />
 
       <!-- <v-col class="mr-auto ml-auto" cols="5"><button class="businessmodelcanvas_view2__button mb-10">SAVE</button></v-col> -->
     </v-col>
@@ -200,10 +56,35 @@
 
 
 <script lang="ts">
-import Vue from 'vue'
-import Component from 'vue-class-component'
-@Component
-export default class businessmodelcanvas_view2 extends Vue{
-    
+import Vue from "vue";
+import Component from "vue-class-component";
+import { BusinessModelCanvas as BusinessModelCanvasComp } from "./components";
+import { BusinessModelCanvas } from "../../../store/Database/types/utilities";
+@Component({
+  components: {
+    BusinessModelCanvasComp
+  }
+})
+export default class businessmodelcanvas_view2 extends Vue {
+  canvas: BusinessModelCanvas = {
+    problem: "My Pitch is this",
+    solution: "My Pitch is this",
+    innovation: "My Pitch is this",
+    cost: "My Pitch is this"
+  };
+  stars: {
+    problem: number;
+    solution: number;
+    innovation: number;
+    cost: number;
+  } = {
+    problem: 0,
+    solution: 3,
+    innovation: 0,
+    cost: 0
+  };
+  onStarsChanged(stars:any){
+    this.stars=stars
+  }
 }
 </script>
