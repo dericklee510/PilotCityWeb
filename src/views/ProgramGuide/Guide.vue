f3c1617b2b326316a3085cc52154f03eec701934
Juxsta <ericjrreyes2@gmail.com>
2020-01-06 10:39:35 -0800

    :lipstick: style changes

--- src/views/ProgramGuide/Guide.vue
+++ src/views/ProgramGuide/Guide.vue
@@ -1,199 +1,250 @@
 <template>
-  <v-row class="ml-0 mr-0">
+  <v-row
+    class="ml-0 mr-0"
+    style="height: 100%"
+  >
     <v-col
-      cols="2"
+      cols="12"
       class="pl-12 pr-12 guide__bar"
     >
       <!-- MAIN GROUP -->
-      <v-row class="guide__mainrow">
-        <span class="guide__bigdot guide__activebigdot guide__background_blue"><i class="guide__icon_white fas fa-chalkboard-teacher guide__programicon" /></span>
+      <v-col class="guide__mainrow">
+        <span class="guide__bigdot guide__bigdot--active guide__background_blue"><i class="guide__icon_white fas fa-chalkboard-teacher guide__programicon" /></span>
 
-        <span class="guide__maintext guide__activemaintext">Externship Day</span>
-      </v-row>
-
-
-
-
-
-      <!-- SUB GROUP -->
-      <v-row class="guide__bridge" />
-      <v-row class="guide__subrow">
-        <span class="guide__smalldot" />
-
-        <span class="guide__smalldotfilled guide__background_blue" />
-
-        <span class="guide__subtext">Agenda</span>
-      </v-row>
-
-
-
+        <span class="guide__maintext guide__maintext--active">Externship Day</span>
+      </v-col>
 
 
 
 
 
       <!-- SUB GROUP -->
-      <v-row class="guide__bridge" />
-      <v-row class="guide__subrow">
+      <v-col>
+        <v-col class="guide__bridge" />
+      </v-col>
+      <v-col class="guide__subrow">
+        <span>
+          <div class="guide__smalldot" />
+          
+          <div class="guide__smalldotfilled guide__background_blue" />
+          
+          <div class="guide__subtext">
+            Agenda
+          </div>
+        </span>
+      </v-col>
+
+
+
+
+
+
+
+
+      <!-- SUB GROUP -->
+      <v-col>
+        <v-col class="guide__bridge" />
+      </v-col>
+      <v-col class="guide__subrow">
         <span class="guide__smalldot" />
         <span class="guide__smalldotfilled guide__background_blue" />
         <span class="guide__subtext">Brief</span>
-      </v-row>
+      </v-col>
 
       <!-- SUB GROUP -->
-      <v-row class="guide__bridge" />
-      <v-row class="guide__subrow">
+      <v-col>
+        <v-col class="guide__bridge" />
+      </v-col>
+      <v-col class="guide__subrow">
         <span class="guide__smalldot" />
         <span class="guide__smalldotfilled guide__background_blue" />
         <span class="guide__subtext">Manage</span>
-      </v-row>
+      </v-col>
 
       <!-- MAIN GROUP -->
-      <v-row class="guide__bridge" />
-      <v-row class="guide__mainrow">
-        <span class="guide__bigdot  guide__background_green guide__activebigdot"><i class="fas fa-project-diagram guide__icon_white guide__programicon" /></span>
-        <span class="guide__maintext guide__activemaintext ">Project</span>
-      </v-row>
+      <v-col>
+        <v-col class="guide__bridge" />
+      </v-col>
+      <v-col class="guide__mainrow">
+        <span class="guide__bigdot  guide__background_green guide__bigdot--active"><i class="fas fa-project-diagram guide__icon_white guide__programicon" /></span>
+        <span class="guide__maintext guide__maintext--active ">Project</span>
+      </v-col>
       <!-- SUB GROUP -->
-      <v-row class="guide__bridge" />
-      <v-row class="guide__subrow">
+      <v-col>
+        <v-col class="guide__bridge" />
+      </v-col>
+      <v-col class="guide__subrow">
         <span class="guide__smalldot" />
         <span class="guide__smalldotfilled guide__background_green" />
         <span class="guide__subtext">Intro Video</span>
-      </v-row>
+      </v-col>
       <!-- SUB GROUP -->
-      <v-row class="guide__bridge" />
-      <v-row class="guide__subrow">
+      <v-col>
+        <v-col class="guide__bridge" />
+      </v-col>
+      <v-col class="guide__subrow">
         <span class="guide__smalldot" />
         <span class="guide__smalldotfilled guide__background_green" />
         <span class="guide__subtext">Train</span>
-      </v-row>
+      </v-col>
       <!-- SUB GROUP -->
-      <v-row class="guide__bridge" />
-      <v-row class="guide__subrow">
+      <v-col>
+        <v-col class="guide__bridge" />
+      </v-col>
+      <v-col class="guide__subrow">
         <span class="guide__smalldot" />
         <span class="guide__smalldotfilled guide__background_green" />
         <span class="guide__subtext">Practice</span>
-      </v-row>
+      </v-col>
 
       <!-- SUB GROUP -->
-      <v-row class="guide__bridge" />
-      <v-row class="guide__subrow">
+      <v-col>
+        <v-col class="guide__bridge" />
+      </v-col>
+      <v-col class="guide__subrow">
         <span class="guide__smalldot" />
         <span class="guide__smalldotfilled guide__background_green" />
         <span class="guide__subtext">Research</span>
-      </v-row>
+      </v-col>
 
       <!-- SUB GROUP -->
-      <v-row class="guide__bridge" />
-      <v-row class="guide__subrow">
+      <v-col>
+        <v-col class="guide__bridge" />
+      </v-col>
+      <v-col class="guide__subrow">
         <span class="guide__smalldot" />
         <span class="guide__smalldotfilled guide__background_green" />
         <span class="guide__subtext">Ideate</span>
-      </v-row>
+      </v-col>
 
       <!-- SUB GROUP -->
-      <v-row class="guide__bridge" />
-      <v-row class="guide__subrow">
+      <v-col>
+        <v-col class="guide__bridge" />
+      </v-col>
+      <v-col class="guide__subrow">
         <span class="guide__smalldot" />
         <span class="guide__smalldotfilled guide__background_green" />
         <span class="guide__subtext">Prepare</span>
-      </v-row>
+      </v-col>
 
       <!-- SUB GROUP -->
-      <v-row class="guide__bridge" />
-      <v-row class="guide__subrow">
+      <v-col>
+        <v-col class="guide__bridge" />
+      </v-col>
+      <v-col class="guide__subrow">
         <span class="guide__smalldot" />
         <span class="guide__smalldotunfilled" />
         <span class="guide__subtext">Hack Day</span>
-      </v-row>
+      </v-col>
 
       <!-- SUB GROUP -->
-      <v-row class="guide__bridge" />
-      <v-row class="guide__subrow">
+      <v-col>
+        <v-col class="guide__bridge" />
+      </v-col>
+      <v-col class="guide__subrow">
         <span class="guide__smalldot" />
         <span class="guide__smalldotunfilled" />
         <span class="guide__subtext">Reflection</span>
-      </v-row>
+      </v-col>
 
       <!-- SUB GROUP -->
-      <v-row class="guide__bridge" />
-      <v-row class="guide__subrow">
+      <v-col>
+        <v-col class="guide__bridge" />
+      </v-col>
+      <v-col class="guide__subrow">
         <span class="guide__smalldot" />
         <span class="guide__smalldotunfilled" />
         <span class="guide__subtext">Design & Prototype</span>
-      </v-row>
+      </v-col>
 
       <!-- SUB GROUP -->
-      <v-row class="guide__bridge" />
-      <v-row class="guide__subrow">
+      <v-col>
+        <v-col class="guide__bridge" />
+      </v-col>
+      <v-col class="guide__subrow">
         <span class="guide__smalldot" />
         <span class="guide__smalldotunfilled" />
         <span class="guide__subtext">Package</span>
-      </v-row>
+      </v-col>
 
       <!-- SUB GROUP -->
-      <v-row class="guide__bridge" />
-      <v-row class="guide__subrow">
+      <v-col>
+        <v-col class="guide__bridge" />
+      </v-col>
+      <v-col class="guide__subrow">
         <span class="guide__smalldot" />
         <span class="guide__smalldotunfilled" />
         <span class="guide__subtext">Demo Day</span>
-      </v-row>
+      </v-col>
 
       <!-- SUB GROUP -->
-      <v-row class="guide__bridge" />
-      <v-row class="guide__subrow">
+      <v-col>
+        <v-col class="guide__bridge" />
+      </v-col>
+      <v-col class="guide__subrow">
         <span class="guide__smalldot" />
         <span class="guide__smalldotunfilled" />
         <span class="guide__subtext">Exit Form</span>
-      </v-row>
+      </v-col>
 
 
 
       <!-- MAIN GROUP -->
-      <v-row class="guide__bridge" />
-      <v-row class="guide__mainrow">
+      <v-col>
+        <v-col class="guide__bridge" />
+      </v-col>
+      <v-col class="guide__mainrow">
         <span class="guide__bigdot"><i class="fas fa-seedling guide__programicon guide__icon_gray" /></span>
         <span class="guide__maintext">Internship</span>
-      </v-row>
+      </v-col>
 
       <!-- SUB GROUP -->
-      <v-row class="guide__bridge" />
-      <v-row class="guide__subrow">
+      <v-col>
+        <v-col class="guide__bridge" />
+      </v-col>
+      <v-col class="guide__subrow">
         <span class="guide__smalldot" />
         <span class="guide__smalldotunfilled" />
         <span class="guide__subtext">Interview Offer</span>
-      </v-row>
+      </v-col>
 
       <!-- SUB GROUP -->
-      <v-row class="guide__bridge" />
-      <v-row class="guide__subrow">
+      <v-col>
+        <v-col class="guide__bridge" />
+      </v-col>
+      <v-col class="guide__subrow">
         <span class="guide__smalldot" />
         <span class="guide__smalldotunfilled" />
         <span class="guide__subtext">First Interview</span>
-      </v-row>
+      </v-col>
       <!-- SUB GROUP -->
-      <v-row class="guide__bridge" />
-      <v-row class="guide__subrow">
+      <v-col>
+        <v-col class="guide__bridge" />
+      </v-col>
+      <v-col class="guide__subrow">
         <span class="guide__smalldot" />
         <span class="guide__smalldotunfilled" />
         <span class="guide__subtext">Final Interview</span>
-      </v-row>
+      </v-col>
       <!-- SUB GROUP -->
-      <v-row class="guide__bridge" />
-      <v-row class="guide__subrow">
+      <v-col>
+        <v-col class="guide__bridge" />
+      </v-col>
+      <v-col class="guide__subrow">
         <span class="guide__smalldot" />
         <span class="guide__smalldotunfilled" />
         <span class="guide__subtext">Position Offer</span>
-      </v-row>
+      </v-col>
 
       <!-- SUB GROUP -->
-      <v-row class="guide__bridge" />
-      <v-row class="guide__subrow">
+      <v-col>
+        <v-col class="guide__bridge" />
+      </v-col>
+      <v-col class="guide__subrow">
         <span class="guide__smalldot" />
         <span class="guide__smalldotunfilled" />
         <span class="guide__subtext">Next Program</span>
-      </v-row>
+      </v-col>
     </v-col>
   </v-row>
 </template>
