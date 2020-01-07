8df01a1fb634f0c6b1a63254a290e575803e15da
Juxsta <ericjrreyes2@gmail.com>
2020-01-06 16:50:54 -0800

    :construction: frontend 1/2

--- src/views/ProgramGuide/Posthack/Posthack.vue
+++ src/views/ProgramGuide/Posthack/Posthack.vue
@@ -29,74 +29,76 @@
         Now that you’ve received feedback from your employer, it is time to evolve, iterate or pivot your project if necessary.
       </v-row>
 
-
-
       <v-row id="posthack__check1">
-        <span class="posthack__wholeline"><input
-                                            class="posthack__check"
-                                            type="checkbox"
-                                          >Update Business Model Canvas 
-
+        <span class="posthack__wholeline">
+          <input
+            v-model="canvas"
+            class="posthack__check"
+            type="checkbox"
+          >
+          Update Business Model Canvas
           <span class="posthack__error">*</span>
-          <span><button class="posthack__exporticon"><img src="@/assets/greenexport.png"></button></span>
-
+          <span>
+            <button class="posthack__exporticon">
+              <img src="@/assets/greenexport.png">
+            </button>
+          </span>
         </span>
       </v-row>
 
       <v-row>
-        <span class="posthack__descriptionline">
-          Description of business model canvas
-        </span>
+        <span class="posthack__descriptionline">Description of business model canvas</span>
       </v-row>
-
-
 
       <v-row id="posthack__check2">
-        <span class="posthack__wholeline"><input
-                                            class="posthack__check"
-                                            type="checkbox"
-                                          >Update One Sentence Pitch   
-
+        <span class="posthack__wholeline">
+          <input
+            v-model="sentencePitch"
+            class="posthack__check"
+            type="checkbox"
+          >
+          Update One Sentence Pitch
           <span class="posthack__error">*</span>
-          <span><button class="posthack__exporticon"><img src="@/assets/greenexport.png"></button></span>
-
+          <span>
+            <button class="posthack__exporticon">
+              <img src="@/assets/greenexport.png">
+            </button>
+          </span>
         </span>
       </v-row>
 
       <v-row>
-        <span class="posthack__descriptionline">
-          Description of one sentence pitch
-        </span>
+        <span class="posthack__descriptionline">Description of one sentence pitch</span>
       </v-row>
-
-
 
       <v-row id="posthack__check2">
-        <span class="posthack__wholeline"><input
-                                            class="posthack__check"
-                                            type="checkbox"
-                                          >Update Written Elevator Pitch    
-
+        <span class="posthack__wholeline">
+          <input
+            v-model="elevatorPitch"
+            class="posthack__check"
+            type="checkbox"
+          >
+          Update Written Elevator Pitch
           <span class="posthack__error">*</span>
-          <span><button class="posthack__exporticon"><img src="@/assets/greenexport.png"></button></span>
-
+          <span>
+            <button class="posthack__exporticon">
+              <img src="@/assets/greenexport.png">
+            </button>
+          </span>
         </span>
       </v-row>
 
       <v-row>
-        <span class="posthack__descriptionline">
-          Description of written elevator pitch
-        </span>
+        <span class="posthack__descriptionline">Description of written elevator pitch</span>
       </v-row>
 
-
-
-
-        
       <v-row>
-        <button class="posthack__acknowledgebutton">
+        <v-btn
+          class="posthack__acknowledgebutton"
+          :disabled="!pivotReady"
+        >
           ACKNOWLEDGE
-        </button>
+        </v-btn>
       </v-row>
     </v-col>
   </v-row>
@@ -105,10 +107,15 @@
 
 
 <script lang="ts">
-import Vue from 'vue'
-import Component from 'vue-class-component'
+import Vue from "vue";
+import Component from "vue-class-component";
 @Component
-export default class posthack extends Vue{
-    
+export default class posthack extends Vue {
+  canvas: boolean = false;
+  sentencePitch: boolean = false;
+  elevatorPitch: boolean = false;
+  get pivotReady(){
+    return [this.canvas, this.sentencePitch, this.elevatorPitch].every(val => val)
+  }
 }
 </script>
