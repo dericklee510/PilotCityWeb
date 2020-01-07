dd8d002d95b8e4bf2c62bdcb8f025fec050f7485
Jerold Inocencio <jerold.inocencio@gmail.com>
2020-01-07 08:49:05 -0800

    :sparkles: Managing init

---
+++ src/views/ProgramGuide/views/Externship/Manage/Manage.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-row>
+    <span class="manageclass__topbutton">
+      <button class="manageclass__classesbutton">CLASSES</button>
+
+      <button class="manageclass__studentsbutton">STUDENTS</button>
+
+      <button class="manageclass__teambutton">TEAMS</button>
+    </span>
+    <router-view />
+  </v-row>
+</template>
+
+
+
+
+
+
+<script lang="ts">
+import Vue from 'vue'
+import Component from 'vue-class-component'
+@Component
+export default class Manage extends Vue{
+    
+}
+</script>
