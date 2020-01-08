33eae925d24b179e4309e24c427335bb223a3d66
sayakura <linra.kura@gmail.com>
2020-01-07 17:16:24 -0800

    wtf

--- src/store/Database/index.ts
+++ src/store/Database/index.ts
@@ -1,40 +1,84 @@
 import { AgendaTemplate, NamedLink } from './types/utilities';
 /* eslint-disable-next-line */
-import { Module, VuexModule, Action } from "vuex-module-decorators" //action unused
-import firestore from '@/firebase/init'
-
-
+import { Module, VuexModule, Action, Mutation } from "vuex-module-decorators" //action unused
+import { firebaseApp as fb } from '@/firebase/init'
+import { EmployerProgram } from './types/types' 
 
 @Module({ namespaced: true, name: 'Fb' })
 export default class Fb extends VuexModule {
-    public firestore = firestore
+    public firestore = fb.firestore()
+    public storage = fb.storage()
+    private employerProgram: EmployerProgram | null = null;
 
     get userDocRef() { //no return type
         let user = this.context.rootState.Auth.user as firebase.User | null
-        return (user) ? firestore.collection('users').doc(user.uid) : null
+        return (user) ? this.firestore.collection('users').doc(user.uid) : null
     }
-    
-    public 
+    get storageRef() {
+        let user = this.context.rootState.Auth.user as firebase.User | null
+        return (user) ? this.storage.ref() : null
+    }
+    get getCurrentEmployerProgram() {
+        return this.employerProgram;
+    }
+
+    @Mutation
+    initEmployerProgram(program: EmployerProgram | null) {
+        this.employerProgram = program;
+    }
+
+    @Mutation
+    updateEmployerProgram(property: any) {
+        
+    }
+
+    @Action({ commit: 'initEmployerProgram'})
+    async fetchEmployerProgram() {
+        let user = this.context.rootState.Auth.user as firebase.User | null
+        if (!user) throw new Error('Not authorized')
+        const snapshot = await this.firestore.collection('EmployerProgram').doc(user.uid).get();
+        if (!snapshot.exists) { 
+            return null ;
+        } else {
+            return snapshot.data()
+        }
+    }
+
+    createProgramBrief = async (file:File):Promise<void> => {
+        if (!this.getCurrentEmployerProgram)
+            throw new Error('Current EmployerProgram is not initialized')
+        if (!this.storageRef)
+            throw new Error('Can\'t find cloud storage ref')
+        // File name
+        // Store file in FB Storage
+        const fileName = file.name; // should validate the name of the file on the frontend
+        const filePath = `program_briefs/${fileName}`;
+        const fileRef = this.storageRef.child(filePath);
+        const snapshot = await fileRef.put(file);
+        
+    }
+        
+    reuploadProgramBrief = async (file:File, uid:string):Promise<void> => {
+        // delete file in FB Storage
+        // upload new file
+        await this.createProgramBrief(file);
+    }
+
+    deleteProgramBrief = async (empProgramId:string):Promise<void> => {
+        if (!this.storageRef)
+            return;
+        let path: string;
+        const snapshot = await this.firestore.collection('EmployerProgram').doc(empProgramId).get();
+        if (snapshot.exists) {
+            const d = snapshot.data()!
+            // get that stupid ass link 
+            path = "temp"
+        } else {
+            throw new Error("err msg")
+        }
+        // call a rest api to delete that file: https://cloud.google.com/storage/docs/deleting-objects
+    }
 }    
-
-
-
-
-
-const createProgramBrief = async (file:File):Promise<void> => {
-    // File name
-    // Store file in FB Storage
-    // Create Employer Program 
-    return
-}
-
-const reuploadProgramBrief = async (file:File, uid:string):Promise<void> => {
-    // delete file in FB Storage
-    // upload new file
-    return 
-}
-
-const deleteProgramBrief = async (uid:string):Promise<void> => {}
 
 const renameBrief = async (name:string, uid:string):Promise<void> => {}
 
