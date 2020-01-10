7c6d02c29d4be6155dbb3e0888a5b99782c74e84
Prajit Saravanan <prajit.saravana@gmail.com>
2020-01-10 01:11:59 -0800

    have some more editing to work on

--- src/views/ProgramGuide/ManageStudents/managestudents.vue
+++ src/views/ProgramGuide/ManageStudents/managestudents.vue
@@ -16,65 +16,7 @@
 
            <span class="managestudents__title"> Manage Students</span>
 
-            <v-row class="managestudents__headtitle">
-
-                <span class="managestudents__teamname">Team Name</span>
-
-            </v-row>
-
         </v-row>
-
-
-
-        <v-row>
-
-            <span class="managestudents__wholeline">
-
-                <input class="managestudents__classinput" placeholder="TEAM NAME 1">
-                <button class="managestudents__renamebutton">RENAME</button>
-                <button class="managestudents__deletebutton">DELETE</button>
-
-            </span>
-
-            <v-row>
-                <span class="managestudents__wholeline2">
-
-                    <input class="managestudents__classinput" placeholder="TEAM NAME 2">
-                    <button class="managestudents__renamebutton">RENAME</button>
-                    <button class="managestudents__deletebutton">DELETE</button>
-
-                </span>
-            </v-row>
-
-        </v-row>
-
-
-        <v-row class="managestudents__titlemain">
-
-           <span class="managestudents__title1"> Create Team</span>
-
-           <v-row class="managestudents__headtitle">
-
-                <span class="managestudents__teamname">Team Name</span>
-
-            </v-row>
-
-        </v-row>
-         
-
-         <v-row>
-
-            <span class="managestudents__wholeline3">
-
-                <input class="managestudents__classinput" placeholder="TEAM NAME 3">
-                <button class="managestudents__createbutton">CREATE</button>
-
-            </span>
-
-         </v-row>
-
-
-
 
 
      </v-row>
