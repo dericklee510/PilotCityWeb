2dd245ebaa8932851d0a4ca41089157f8dd574e4
Juxsta <ericjrreyes2@gmail.com>
2020-01-15 14:40:52 -0800

    type updates

--- src/environment.d.ts
+++ src/environment.d.ts
@@ -1,3 +1,4 @@
+
 export { };
 
 declare global {
@@ -12,16 +13,24 @@ declare global {
       VUE_APP_TEST_DB_KEY: string
       VUE_APP_MAPS_API_KEY: string
       //Server Endpoints
-      VUE_APP_VERIFY_LINK_REST:string
+      VUE_APP_VERIFY_LINK_REST: string
     }
   }
   namespace firebase.firestore {
+    type Collection = | "GeneralUser"
+      | "EmployerProgram"
+      | "TeacherProgramData"
+      | "Project"
+      | "Classroom";
     interface DocumentSnapshot {
-      data<RetrievedClass>(options?: SnapshotOptions):(RetrievedClass & Record<string,any> )
+      data<RetrievedClass>(options?: SnapshotOptions): (RetrievedClass & Record<string, any>)
     }
-    interface DocumentReference{
-      update<UpdateClass>(data:Partial<UpdateClass>):Promise<void>
-      set<UpdateClass>(data:UpdateClass):Promise<void>
+    interface DocumentReference {
+      update<UpdateClass>(data: Partial<UpdateClass>): Promise<void>
+      set<UpdateClass>(data: UpdateClass): Promise<void>
+    }
+    interface Firestore {
+      collection(collectionPath: Collection): CollectionReference
     }
   }
 }
