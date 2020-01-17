6dace74451b0f23539e2f0fe5281b46a07b1ee4c
Juxsta <ericjrreyes2@gmail.com>
2020-01-16 16:26:20 -0800

    merge fix

--- src/views/Test/Test.vue
+++ src/views/Test/Test.vue
@@ -1,30 +1,3 @@
-<<<<<<< HEAD
-<template>
-  <div />
-</template>
-<script lang="ts">
-import Vue from "vue";
-import Component from "vue-class-component";
-import { FbStore } from "@/store";
-import { GeneralUser } from "../../store/Database/types/types";
-@Component
-export default class Test extends Vue {
-  async created() {
-    this.createUserProfile();
-  }
-
-  createUserProfile() {
-    let uid = (FbStore.FBUser as firebase.User).uid;
-
-    // FbStore.firestore
-    //   .collection("GeneralUser")
-    //   .doc(uid)
-    //   .set<GeneralUser>({
-    //     firstName: "Taisei"
-    //   });
-  }
-}
-=======
 <template>
   <div />
 </template>
@@ -51,5 +24,4 @@ export default class Test extends Vue {
       });
   }
 }
->>>>>>> 2dd245ebaa8932851d0a4ca41089157f8dd574e4
 </script>
