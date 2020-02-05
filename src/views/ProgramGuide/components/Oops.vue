bbbed3c98a10a95fe779a488e7018554157a0c29
Juxsta <ericjrreyes2@gmail.com>
2020-02-04 21:15:20 -0800

    oops

---
+++ src/views/ProgramGuide/components/Oops.vue
@@ -0,0 +1,52 @@
+<template>
+  <v-overlay absolute>
+    <v-row
+      align="center"
+      justify="center"
+    >
+      <v-col
+        cols="8"
+      >
+        <v-card
+          class="elevation-8"
+          height="200"
+          width="400"
+          dark
+        >
+          <v-row
+            align="center"
+            justify="center"
+          >
+            <v-alert
+              width="200"
+            >
+              There is nothing here yet...<i class="far fa-grin-beam-sweat" />
+            </v-alert>
+          </v-row>
+          <v-row
+            align="center"
+            justify="center"
+          >
+            <v-progress-circular indeterminate="" />
+          </v-row>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-overlay>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+@Component<Oops>({
+  components: {
+  },
+  subscriptions() {
+    return {
+      
+    };
+  }
+})
+export default class Oops extends Vue {
+}
+</script>
