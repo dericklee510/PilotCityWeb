74b6963f1b1e52f1e7c3614c4fec69eadd770d49
jinocenc <jerold.inocencio@gmail.com>
2020-03-12 17:02:52 -0700

    :sparkles: init kuras api

--- src/views/Test/Test.vue
+++ src/views/Test/Test.vue
@@ -1,28 +1,61 @@
 <template>
-  <div />
+  <v-container fluid>
+    <v-row
+      no-gutters
+      justify="center"
+      align="center"
+    >
+      <v-col cols="12">
+        <h2>Enter Your Phone Number For Verification</h2>
+        <v-text-field
+          v-model="phone"
+          @keyup.enter="sendCode(phone)"
+        />
+        <h1> your number is: {{ phone }} </h1>
+      </v-col>
+      <v-col cols="12">
+        <h2>Enter Verification Code</h2>
+        <v-text-field
+          :code="code"
+          @keyup.enter="verifyPhone(code)"
+        />
+      </v-col>
+    </v-row>    
+  </v-container>
 </template>
 <script lang="ts">
 import Vue from "vue";
 import Component from "vue-class-component";
-import { FbStore } from "@/store";
-import { GeneralUser } from "../../store/Database/types/types";
-@Component
-export default class Test extends Vue {
-  async created() {
-    this.createUserProfile();
+import { SMSsendCode, isValidSMS } from "@/api"
+
+
+@Component({
+  async beforeRouteEnter(to, from, next) {
+    if (process.env.NODE_ENV === 'development') next()
   }
+})
+export default class Test extends Vue {
+  public code: string = ""
+  public phone: string = ""
 
-  createUserProfile() {
-    let uid = (FbStore.FBUser as firebase.User).uid;
-
-    FbStore.firestore
-      .collection("GeneralUser")
-      .doc(uid)
-      .update<GeneralUser>({
-        firstName: "Taisei",
-        lastName: "Tateno",
-        
-      });
+  async sendCode(phone: string){
+    try {
+      console.log(phone, {to: phone})
+      const data = await SMSsendCode({to: phone});
+      console.info('%c Code has been sent!', 'background: green; color: white; display: block; text-align: center')
+    } catch(err) {
+      console.error(`${err} \nThere was an error sending code to ${phone} \n Please try again later`)
+    }
+  }
+  async verifyPhone( phone: string, code: string,) {
+    try {
+      console.log({to: phone, code: code})
+      const val = await isValidSMS({to: phone, code: code});
+      console.log(val)
+      console.info('%c Code has been verified!', 'background: green; color: white; display: block; text-align: center')
+    } catch(err) {
+      console.error(`${err} \n There was an error verifying code: ${code} \n Please try again later`)
+    }
   }
 
  
